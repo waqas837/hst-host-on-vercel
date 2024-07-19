3c85403e95f8164cef2e3041cdd2a306b8638478
unknown <mwaqas3030@gmail.com>
2024-07-19 10:14:50 +0500

    Asdf

--- Model/userSchema.js
+++ Model/userSchema.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 mongoose
   .connect(
-    "mongodb://127.0.0.1:27017/hst",
+    "mongodb+srv://bughlani:bughlani@cluster0.9qki3lg.mongodb.net/hst?retryWrites=true&w=majority&appName=Cluster0",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
