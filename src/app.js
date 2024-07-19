231e2c722110b513c62bfe4c04a3b19b99d323f6
unknown <mwaqas3030@gmail.com>
2024-07-19 08:30:24 +0500

    Asdf

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const router = require("../routes/routes");
+const router = require("../routes");
 const app = express();
 const path = require("path");
 app.use(cors());
